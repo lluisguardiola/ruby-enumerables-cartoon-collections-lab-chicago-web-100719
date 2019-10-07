c5e6820cd64e17d74326b60de80ccca785fb3c3c
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-07 07:11:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(calls)
   calls.map {|n| "#{n.capitalize}!"}
 end
 
-def long_planeteer_calls(calls)
-  calls.any? {|n| n.length > 4}
+def long_planeteer_calls(words)
+  words.any? {|n| n.length > 4}
 end
 
 def find_the_cheese# code an argument here
