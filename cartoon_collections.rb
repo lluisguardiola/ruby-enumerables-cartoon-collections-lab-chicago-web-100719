f85f3912aff3a049507f8beee6ce0132233e0048
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-07 02:31:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(name_list)
-  name_list.each_with_index {|item| puts "#{item}"}
+  name_list.each_with_index {|item, index| puts "#{index+1}#{item}"}
 end
 
 def summon_captain_planet# code an argument here
