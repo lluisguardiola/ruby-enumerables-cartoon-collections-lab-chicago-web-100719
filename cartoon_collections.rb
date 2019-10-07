22f9e50eca72b1eff3c63c52580072aedf1ab9c0
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-07 07:15:10 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name_list)
+  name_list.each_with_index {|item, index| puts "#{index+1}#{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map {|n| "#{n.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? {|n| n.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  ingredients.find {|n| cheese_types.include?(n)}
+  
 end
